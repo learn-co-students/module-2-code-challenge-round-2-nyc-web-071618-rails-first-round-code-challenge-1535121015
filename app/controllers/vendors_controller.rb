50fbe2f5242274d163a0181bfc437eb7aba1142c
bwadlow <bwadlow@gmail.com>
2018-08-24 12:06:01 -0400

    done

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -4,4 +4,24 @@ class VendorsController < ApplicationController
     @vendors = Vendor.all
   end
 
-end
+  def show
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def edit
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def update
+    @vendor = Vendor.find(params[:id])
+    @vendor = 
+    #code
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name)
+  end
+
+end#end of class
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,8 @@
 class Vendor < ApplicationRecord
+  has_many :vendor_sweets
+  has_many :sweets, through: :vendor_sweets
+
+  validates :sweets, uniqueness: :true
+
 
 end
