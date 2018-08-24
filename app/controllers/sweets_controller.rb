46404a3cad7ee2734470ef671ec1571c9e7cce95
TylerNodell <Tyler.l.nodell@gmail.com>
2018-08-24 12:05:11 -0400

    I dont understand how to connect a form through a join table

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,5 +1,15 @@
 class SweetsController < ApplicationController
+  before_action(:find_sweet, only: [:show])
   def index
     @sweets = Sweet.all
+  end
+
+  def show
+    
+  end
+  private
+
+  def find_sweet
+    @sweet = Sweet.find(params[:id])
   end
 end
---
+++ db/migrate/20180824144424_vendor_sweets.rb
@@ -0,0 +1,4 @@
+class VendorSweets < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
